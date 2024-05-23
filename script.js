a362d9387ed90abd6cad7c1805db295daa6a0ddf
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-05-23 19:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	let arr=[];
+	let s="";
+	for(int i=0;i<str.length;i++){
+		if(i%size==0){
+			arr.push(s);
+			s="";
+		}
+		s+=str.charAt(i);
+	}
+	return arr;
 }
 
 // Do not change the code below
