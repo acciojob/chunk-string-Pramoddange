c98b8d026bdc1ec851d9cfb496cfa42c6f5dced1
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-05-30 15:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ function stringChop(str, size) {
 
   return result;
 }
-}
+
 
 // Do not change the code below
-/*
+
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
